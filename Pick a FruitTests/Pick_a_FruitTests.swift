aaca9245a0ce77441768b3869bf86a282270ad82
Farrukh Askari <farrukh.askari01@gmail.com>
2016-01-24 07:58:21 -0800

    Initial Commit

---
+++ Pick a FruitTests/Pick_a_FruitTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pick_a_FruitTests.swift
+//  Pick a FruitTests
+//
+//  Created by Syed Askari on 24/01/2016.
+//  Copyright © 2016 Syed Askari. All rights reserved.
+//
+
+import XCTest
+@testable import Pick_a_Fruit
+
+class Pick_a_FruitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
